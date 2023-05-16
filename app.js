20a27cd9fb9394c139963201388aa0cf1156535f
Waleed-abid1 <waleed@fixfirst.io>
2023-05-16 11:48:10 -0700

    Completed App

--- app.js
+++ app.js
@@ -1,264 +1,140 @@
-require('dotenv').config()
-require('./src/connect/mongodb')
-const bodyParser = require('body-parser')
-const express = require('express')
-const session = require('express-session')
-var MemoryStore = require('memorystore')(session)
-const UserService = require('./src/user')
-const Stripe = require('./src/connect/stripe')
-const setCurrentUser = require('./src/middleware/setCurrentUser')
-const hasPlan = require('./src/middleware/hasPlan')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const session = require("express-session");
+const app = express();
+require("dotenv").config();
+const Publishable_Key = process.env.Publishable_Key;
+const Secret_Key = process.env.Secret_Key;
+console.log(Publishable_Key, Secret_Key);
+const stripe = require("stripe")(Secret_Key);
 
-const app = express()
-app.use(session({
-  saveUninitialized: false,
-  cookie: { maxAge: 86400000 },
-  store: new MemoryStore({
-    checkPeriod: 86400000 // prune expired entries every 24h
-  }),
-  resave: false,
-  secret: 'keyboard cat'
-}))
+const port = process.env.PORT || 3000;
 
-app.use('/webhook', bodyParser.raw({ type: 'application/json' }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// View Engine Setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
-app.engine('html', require('ejs').renderFile)
-
-const productToPriceMap = {
-  basic: process.env.PRODUCT_BASIC,
-  pro: process.env.PRODUCT_PRO
-}
-
-app.get('/none', [setCurrentUser, hasPlan('none')], async function (
-  req,
-  res,
-  next
-) {
-  res.status(200).render('none.ejs')
-})
-
-app.get('/basic', [setCurrentUser, hasPlan('basic')], async function (
-  req,
-  res,
-  next
-) {
-  res.status(200).render('basic.ejs')
-})
-
-app.get('/pro', [setCurrentUser, hasPlan('pro')], async function (
-  req,
-  res,
-  next
-) {
-  res.status(200).render('pro.ejs')
-})
-
-app.get('/', function (req, res) {
-  res.render('login.ejs')
-})
-
-app.get('/account', async function (req, res) {
-  let { email } = req.session
-  let customer = await UserService.getUserByEmail(email)
-  if (!customer) {
-    res.redirect('/')
-  } else {
-    res.render('account.ejs', { customer })
-  }
-})
-
-app.post('/login', async function (req, res) {
-  const { email } = req.body
-  console.log('email', email)
-
-  let customer = await UserService.getUserByEmail(email)
-  let customerInfo = {}
-
-  if (!customer) {
-    console.log(`email ${email} does not exist. Making one. `)
-    try {
-      customerInfo = await Stripe.addNewCustomer(email)
-
-      customer = await UserService.addUser({
-        email: customerInfo.email,
-        billingID: customerInfo.id,
-        plan: 'none',
-        endDate: null
-      })
-
-      console.log(
-        `A new user signed up and addded to DB. The ID for ${email} is ${JSON.stringify(
-          customerInfo
-        )}`
-      )
-
-      console.log(`User also added to DB. Information from DB: ${customer}`)
-    } catch (e) {
-      console.log(e)
-      res.status(200).json({ e })
-      return
-    }
-  } else {
-    const isTrialExpired =
-      customer.plan != 'none' && customer.endDate < new Date().getTime()
-
-    if (isTrialExpired) {
-      console.log('trial expired')
-      customer.hasTrial = false
-      customer.save()
-    } else {
-      console.log(
-        'no trial information',
-        customer.hasTrial,
-        customer.plan != 'none',
-        customer.endDate < new Date().getTime()
-      )
-    }
-
-    customerInfo = await Stripe.getCustomerByID(customer.billingID)
-    console.log(
-      `The existing ID for ${email} is ${JSON.stringify(customerInfo)}`
-    )
-  }
-
-  req.session.email = email
-
-  // res.render('account.ejs', {
-  //   customer,
-  //   customerInfo,
-  //   email
-  // })
-
-  res.redirect('/account')
-})
-
-app.post('/checkout', setCurrentUser, async (req, res) => {
-  const customer = req.user
-  const { product, customerID } = req.body
-
-  const price = productToPriceMap[product]
-
+const getProducts = async () => {
   try {
-    const session = await Stripe.createCheckoutSession(customerID, price)
+    const products = await stripe.products.list();
+    return products.data; // Array of product objects
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
 
-    const ms =
-      new Date().getTime() + 1000 * 60 * 60 * 24 * process.env.TRIAL_DAYS
-    const n = new Date(ms)
+app.get("/", async (req, res) => {
+  try {
+    const products = await getProducts();
 
-    customer.plan = product
-    customer.hasTrial = true
-    customer.endDate = n
-    customer.save()
-
-    res.send({
-      sessionId: session.id
-    })
-  } catch (e) {
-    console.log(e)
-    res.status(400)
-    return res.send({
-      error: {
-        message: e.message
+    // Create a map of product IDs to their respective prices
+    const productPriceMap = {};
+    for (const product of products) {
+      const prices = await stripe.prices.list({ product: product.id });
+      if (prices.data.length > 0) {
+        productPriceMap[product.id] = prices.data[0].unit_amount;
       }
-    })
+    }
+
+    res.render("products", {
+      key: Publishable_Key,
+      products,
+      productPriceMap,
+    });
+  } catch (error) {
+    console.error(error);
+    res.send("Error retrieving products");
   }
-})
+});
 
-app.post('/billing', setCurrentUser, async (req, res) => {
-  const { customer } = req.body
-  console.log('customer', customer)
+// Create a checkout session
+app.post("/create-checkout-session", async (req, res) => {
+  const { product_id } = req.body;
+  try {
+    const product = await stripe.products.retrieve(product_id);
 
-  const session = await Stripe.createBillingSession(customer)
-  console.log('session', session)
+    // Retrieve prices for the product
+    const prices = await stripe.prices.list({
+      product: product.id,
+      active: true,
+    });
 
-  res.json({ url: session.url })
-})
+    if (prices.data.length === 0) {
+      throw new Error("No active prices found for the product");
+    }
+    const price = prices.data[0]; // Assuming you want to use the first price found
 
-app.post('/webhook', async (req, res) => {
-  let event
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: price.id,
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    });
+    res.json({ sessionId: session.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+app.get("/success", (req, res) => {
+  res.send("Success!");
+});
+app.get("/cancel", (req, res) => {
+  res.send("cancelled!");
+});
+// Handle the Stripe webhook endpoint to receive events
+app.post("/stripe-webhook", async (req, res) => {
+  const payload = req.body;
+  const sig = req.headers["stripe-signature"];
+
+  let event;
 
   try {
-    event = Stripe.createWebhook(req.body, req.header('Stripe-Signature'))
+    event = stripe.webhooks.constructEvent(payload, sig, webhookSecret);
   } catch (err) {
-    console.log(err)
-    return res.sendStatus(400)
+    console.error(err);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
-  const data = event.data.object
+  // Handle the event based on its type
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object;
 
-  console.log(event.type, data)
-  switch (event.type) {
-    case 'customer.created':
-      console.log(JSON.stringify(data))
-      break
-    case 'invoice.paid':
-      break
-    case 'customer.subscription.created': {
-      const user = await UserService.getUserByBillingID(data.customer)
+    // Retrieve the session details and store them in your database or perform any other required actions
+    // You can access information such as session ID, customer details, subscription details, etc.
+    const sessionId = session.id;
+    const customerId = session.customer;
+    const subscriptionId = session.subscription;
 
-      if (data.plan.id === process.env.PRODUCT_BASIC) {
-        console.log('You are talking about basic product')
-        user.plan = 'basic'
-      }
+    // Store the information in your database or perform any other desired actions
+    // ...
 
-      if (data.plan.id === process.env.PRODUCT_PRO) {
-        console.log('You are talking about pro product')
-        user.plan = 'pro'
-      }
-
-      user.hasTrial = true
-      user.endDate = new Date(data.current_period_end * 1000)
-
-      await user.save()
-
-      break
-    }
-    case 'customer.subscription.updated': {
-      // started trial
-      const user = await UserService.getUserByBillingID(data.customer)
-
-      if (data.plan.id == process.env.PRODUCT_BASIC) {
-        console.log('You are talking about basic product')
-        user.plan = 'basic'
-      }
-
-      if (data.plan.id === process.env.PRODUCT_PRO) {
-        console.log('You are talking about pro product')
-        user.plan = 'pro'
-      }
-
-      const isOnTrial = data.status === 'trialing'
-
-      if (isOnTrial) {
-        user.hasTrial = true
-        user.endDate = new Date(data.current_period_end * 1000)
-      } else if (data.status === 'active') {
-        user.hasTrial = false
-        user.endDate = new Date(data.current_period_end * 1000)
-      }
-
-      if (data.canceled_at) {
-        // cancelled
-        console.log('You just canceled the subscription' + data.canceled_at)
-        user.plan = 'none'
-        user.hasTrial = false
-        user.endDate = null
-      }
-      console.log('actual', user.hasTrial, data.current_period_end, user.plan)
-
-      await user.save()
-      console.log('customer changed', JSON.stringify(data))
-      break
-    }
-    default:
+    res.sendStatus(200);
+  } else {
+    // Handle other event types if needed
+    res.sendStatus(200);
   }
-  res.sendStatus(200)
-})
+});
 
-const port = process.env.PORT || 4242
-
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log(`Server listening on ${port}`);
+});
